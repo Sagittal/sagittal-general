8bc0e277fd216d3712235d62de1ca679e2dfcf86
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-12-06 11:03:08 -0800

    give general a tmp script

---
+++ spec/helpers/shared/tmp/catchChangingTmp.ts
@@ -0,0 +1,9 @@
+import * as fs from "fs"
+import {deepEquals} from "../../../../src"
+
+const tmp = fs.readFileSync("spec/helpers/shared/tmp/tmp.ts")
+const tmpSnapshot = fs.readFileSync("spec/helpers/shared/tmp/tmpSnapshot.txt")
+
+if (!deepEquals(tmp, tmpSnapshot)) {
+    throw new Error("You have made changes to tmp.ts and should remove them before proceeding.")
+}
