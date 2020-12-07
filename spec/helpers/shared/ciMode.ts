6cb04eef16d8f69b1f9d6dfd7772085eb1472e3c
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-12-07 09:24:35 -0800

    finish off task to break repo down and set it up

--- spec/helpers/shared/ciMode.ts
+++ spec/helpers/shared/ciMode.ts
@@ -1,5 +1,8 @@
 import {program} from "commander"
 
+// TODO: figure out a better solution than tslint disable and hack into commander... what’s happening?
+//  Current solution is less than ideal because it won't catch if in one of your tests when you pass an unknown option
+//  It should fail and help you (maybe... not sure about that though, you should probably experiment to confirm)
 program.allowUnknownOption()
 
 const CI_MODE: boolean = !!process.env.CI || process.argv[2] === "--ci=true"
