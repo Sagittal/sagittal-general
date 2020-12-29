fdcffafc61dd31a0871b531239b9a2b760059d76
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-12-29 10:11:07 -0800

    v1.0.74: allow runScriptAndGetConsoleOutput to throw error

--- src/node/spec/scripts/runScriptAndGetConsoleOutput.ts
+++ src/node/spec/scripts/runScriptAndGetConsoleOutput.ts
@@ -4,9 +4,12 @@ import {count} from "../../../math"
 import {scriptSettings} from "../../scripts"
 import {NPM_SCRIPT_HEADER_LINES_COUNT, SKIP_THE_FINAL_EMPTY_LINE} from "./constants"
 
-const runScriptAndGetConsoleOutput = (script: Io): Io[] => {
+const runScriptAndGetConsoleOutput = (script: Io, {throwError}: {throwError?: boolean} = {}): Io[] => {
     scriptSettings.disableColors = true
-    const consoleOutput: Io = cp.execSync(script, {stdio: ["pipe", "pipe", "inherit"]}).toString() as Io
+    const options: cp.ExecSyncOptionsWithBufferEncoding = throwError ?
+        {stdio: [undefined, undefined, undefined]} :
+        {stdio: ["pipe", "pipe", "inherit"]}
+    const consoleOutput: Io = cp.execSync(script, options).toString() as Io
     const consoleOutputLines: Io[] = split(consoleOutput, NEWLINE)
 
     return consoleOutputLines.slice(
