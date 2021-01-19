bd63862bda4e9e75b17184aba714075c7afa311c
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-01-19 13:08:52 -0800

    v1.0.98: yeah I really meant to add that reporter to the name reporter, oops

--- src/node/spec/reporters/specNameReporter.ts
+++ src/node/spec/reporters/specNameReporter.ts
@@ -1,15 +1,19 @@
-// tslint:disable-next-line no-reaching-imports
-import {CustomReporterResult} from "jasmine-spec-reporter/built/spec-reporter"
 import {LogTarget, saveLog} from "../../scripts"
 
 // This is quite handy when the suite starts to hang, so you can identify where the issue is.
 
 const PRINT_NAMES = process?.argv?.[3] === "--names=true"
 
+const surfaceSpecNameToSpecs = (result: jasmine.CustomReporterResult): void => {
+    (jasmine as unknown as {currentTest: jasmine.CustomReporterResult}).currentTest = result
+}
+
 const specNameReporter: jasmine.CustomReporter = {
-    specStarted(result: CustomReporterResult): void {
+    specStarted(result: jasmine.CustomReporterResult): void {
         if (PRINT_NAMES) saveLog(result.fullName, LogTarget.SPEC)
+        surfaceSpecNameToSpecs(result)
     },
+    specDone: surfaceSpecNameToSpecs,
 }
 
 export {
--- src/node/spec/reporters/specReporter.ts
+++ src/node/spec/reporters/specReporter.ts
@@ -1,17 +1,7 @@
 import {SpecReporter} from "jasmine-spec-reporter"
 import {CI_MODE} from "../ciMode"
 
-let specReporter = {} as jasmine.CustomReporter
-
-const surfaceSpecNameToSpecs = (result: jasmine.CustomReporterResult): void => {
-    (jasmine as unknown as {currentTest: jasmine.CustomReporterResult}).currentTest = result
-}
-
-if (process?.env?.TEST_MODE) {
-    specReporter = new SpecReporter({summary: {displayPending: CI_MODE}})
-    specReporter.specStarted = surfaceSpecNameToSpecs
-    specReporter.specDone = surfaceSpecNameToSpecs
-}
+const specReporter = process?.env?.TEST_MODE ? new SpecReporter({summary: {displayPending: CI_MODE}}) : {}
 
 export {
     specReporter,
