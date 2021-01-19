7a9def4288d3ca4a08f8d7ff6902b4f358b5f29f
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-01-19 13:01:53 -0800

    spec reporter surfaces names to specs

--- src/node/spec/reporters/specReporter.ts
+++ src/node/spec/reporters/specReporter.ts
@@ -1,7 +1,17 @@
 import {SpecReporter} from "jasmine-spec-reporter"
 import {CI_MODE} from "../ciMode"
 
-const specReporter = process?.env?.TEST_MODE ? new SpecReporter({summary: {displayPending: CI_MODE}}) : {}
+let specReporter = {} as jasmine.CustomReporter
+
+const surfaceSpecNameToSpecs = (result: jasmine.CustomReporterResult): void => {
+    (jasmine as unknown as {currentTest: jasmine.CustomReporterResult}).currentTest = result
+}
+
+if (process?.env?.TEST_MODE) {
+    specReporter = new SpecReporter({summary: {displayPending: CI_MODE}})
+    specReporter.specStarted = surfaceSpecNameToSpecs
+    specReporter.specDone = surfaceSpecNameToSpecs
+}
 
 export {
     specReporter,
