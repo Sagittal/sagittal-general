ef7a6acddd6dc21c8c50a5246a793f2295a6ba5d
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2024-10-27 15:38:09 -0700

    fix more ci

--- spec/src/math/numeric/decimal/fromVectorSpec.ts
+++ spec/src/math/numeric/decimal/fromVectorSpec.ts
@@ -1,13 +1,13 @@
-import {computeDecimalFromVector, Decimal, MAX_JS_PRECISION, Vector} from "../../../../../src"
+import { computeDecimalFromVector, Decimal, MAX_JS_PRECISION, Vector } from "../../../../../src"
 
 describe("computeDecimalFromVector", (): void => {
     it("returns the decimal representation of the vector", (): void => {
-        const vector = [-1, -1, 0, 1] as Vector   // 7/6
+        const vector = [-1, -1, 0, 1] as Vector // 7/6
 
         const actual = computeDecimalFromVector(vector)
 
         const expected = 1.166667 as Decimal
-        expect(actual).toBeCloseToTyped(expected)
+        expect(actual).toBeCloseTo(expected)
     })
 
     it("works for a huge vector", (): void => {
@@ -19,7 +19,7 @@ describe("computeDecimalFromVector", (): void => {
         // 2^50508 = 2.648628185 * 10^15204
         // 2.648647428 / 2.648628185 ≈ 1.000007265
         const expected = 1.0000072649602405 as Decimal
-        expect(actual).toBeCloseToTyped(expected, MAX_JS_PRECISION)
+        expect(actual).toBeCloseTo(expected, MAX_JS_PRECISION)
     })
 
     it("works for a huge vector which has no recourse to stay balanced", (): void => {
@@ -28,7 +28,7 @@ describe("computeDecimalFromVector", (): void => {
         const actual = computeDecimalFromVector(vector)
 
         const expected = Infinity as Decimal
-        expect(actual).toBeCloseToTyped(expected)
+        expect(actual).toBeCloseTo(expected)
     })
 
     it("works for this huge vector", (): void => {
@@ -40,23 +40,23 @@ describe("computeDecimalFromVector", (): void => {
         // 2^2108 = 3.725897126 * 10^634
         // 3.726222442 / 3.725897126 = 1.00008731213
         const expected = 1.000087312 as Decimal
-        expect(actual).toBeCloseToTyped(expected)
+        expect(actual).toBeCloseTo(expected)
     })
 
     it("works for this other huge vector", (): void => {
-        const vector = [-1726, 1330] as Vector
+        const vector = [-1726, 1330] as Vector<{}>
 
         const actual = computeDecimalFromVector(vector)
 
         // 3^1330 = 3.726222442 * 10^634
         // 2^1726 = 3.782444079 * 10^519
         // (3.726222442 / 3.782444079) * 10^(634-519) = 0.98513616174 * 10^115
-        const expected = 9.85136161641151e+114 as Decimal
+        const expected = 9.85136161641151e114 as Decimal
         expect(actual).toEqual(expected)
     })
 
     it("works for this other huge vector's inverse", (): void => {
-        const vector = [1726, -1330] as Vector
+        const vector = [1726, -1330] as Vector<{}>
 
         const actual = computeDecimalFromVector(vector)
 
