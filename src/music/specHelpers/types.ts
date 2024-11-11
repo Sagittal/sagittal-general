57ad8c24c8c5060b60a1e9f938cab03271ea2696
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2024-11-10 18:46:30 -0800

    pitch expectation also needs to support scaled vector's new type parameters

--- src/music/specHelpers/types.ts
+++ src/music/specHelpers/types.ts
@@ -1,9 +1,13 @@
 import { NoProperties } from "../../code"
-import { Decimal, Vector, NumericProperties, ScaledVector } from "../../math"
+import { Decimal, Vector, NumericProperties, ScaledVector, Integer } from "../../math"
 import { Cents } from "../types"
 
-type PitchExpectation<T extends NumericProperties = NoProperties> = {
-    pitch: ScaledVector<T>
+type PitchExpectation<
+    T extends NumericProperties = NoProperties,
+    O extends { of?: number } = { of: number },
+    U extends NumericProperties = Integer,
+> = {
+    pitch: ScaledVector<T, O, U>
     cents: Cents
     decimal: Decimal<T>
     vector: Vector<T>
